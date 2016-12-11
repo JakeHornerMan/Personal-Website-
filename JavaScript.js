753a51900b68117be3a72e7bb584aed9f4856464
JakeHornerMan <you@example.com>
2016-12-11 20:23:47 +0000

    page3

--- JavaScript.js
+++ JavaScript.js
@@ -3,5 +3,23 @@ function greet()
 				var name=prompt("What is your name?");
 				
 				alert("Welcome "+name+ ", to my website");
-			}
-			
+			};
+function jokes()
+			{
+				var answer1=prompt ("Whats a pirates favourite letter?");
+				
+				if(answer1=="C" || answer1=="c" || answer1=="the c" || answer1=="the C"){
+					alert("Correct!");
+				}
+				else{
+					alert("The C");
+				}
+				var answer2=prompt (" What can you never eat for breakfast?");
+				
+				if(answer2=="Dinner" || answer2=="dinner" || answer2=="lunch" || answer2=="Lunch"){
+					alert("Correct!");
+				}
+				else{
+					alert("Dinner");
+				}
+			};
